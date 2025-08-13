622d8d306572f016da9bf623871d6f03f132b11a
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2025-08-13 20:48:00 -0300

    Se agrego el estado del tramite en ResumenIntroduccion

---
+++ Enumerable/EstadosEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace CemSys2.Enumerable
+{
+    public enum EstadosIntroduccion
+    {
+        Registrado = 1,
+        Cobrado = 2,
+        Finalizado = 3
+    }
+    
+
+    public class EstadosEnum
+    {
+    }
+}
